3d5d97457e0c9deb849ce074961dda18129b341b
Besnik Rushiti <besnikrushiti12@gmail.com>
2015-06-03 22:48:07 +0200

    test
    test

---
+++ index.php
@@ -0,0 +1,25 @@
+<html> 
+<title></title>
+    <head>
+        <style type="text/css">
+            body
+            {
+                margin:   0;
+                overflow: hidden;
+            }
+
+            #iframe1
+            {
+                height:   100%;
+                left:     0px;
+                position: absolute;
+                top:      0px;
+                width:    100%;
+            }
+        </style>
+    </head>
+
+    <body>
+        <iframe id="iframe1" src="index.php" frameborder="0"></iframe>
+    </body>
+</html>
